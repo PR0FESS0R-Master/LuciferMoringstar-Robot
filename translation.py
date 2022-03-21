64cde56a6fa432d6089467afa1e9cbe9a0ea9c25
PR0FESS0R-Master <98311387+PR0FESS0R-Master@users.noreply.github.com>
2022-03-21 20:23:21 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,30 +2,29 @@ class LuciferMoringstar(object):
 
     DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
 
-    HELP_MSG = """**Help Veno Monuse 🤣**"""
+    HELP_MSG = """**NO Help 🤣**"""
 
     ABOUT_MSG = """
 🤖 My Name [{bot_name}](t.me/{bot_username})
 
-🧑 My Dev : [PR0FESS0R-99](t.me/PR0FESS0R_99)
+🧑 My Dev : [PR0FESS0R-Master](t.me/PR0FESS0R_MASTER)
 
 🧑 My Creator : {dev_name}
 
-📦 My Source : [Click Here](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+📦 My Source : [Click Here](https://github.com/PR0FESS0R-Master/LuciferMoringstar_Robot)
 
 📺 My Tutorial : [Ckick Here](https://youtu.be/FCU_XJmyG_U)
 
-🗃️ My Library : [Pyrogram](https://youtu.be/Af055Eozk9s)
+🗃️ My Library : Pyrogram)
 
-🖇️ My Language : [Python3](https://youtu.be/Af055Eozk9s)
+🖇️ My Language : Python3
 
-🗃️ My Database : [Mongodb](https://youtu.be/gBLTsH-IXr0)
+🗃️ My Database : Mongodb
+🗳️ My Server : Heroku
 
-🗳️ My Server : [Heroku](https://youtu.be/xcZD55td6Vw)
+🏷️ My Updates : [Professor BotZ](https://t.me/The_Professor_100_BotZ)
 
-🏷️ My Updates : [Mo Tech](t.me/Mo_Tech_YT)
-
-🗣️ My Support : [Any Doubt](t.me/Mo_Tech_Group)"""
+🗣️ My Support : [Any Doubt](https://t.me/The_Professor_100_BotZ_Group)"""
 
     FILE_CAPTIONS = """Hello 👋 {mention}\n\n📁Title {title}\n\n🔘Size {size}"""
 
